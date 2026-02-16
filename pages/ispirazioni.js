6d28eadcdeca088cb2bea6acaec0a323961e0366
FraCanta <fcantale14@gmail.com>
2026-02-16 17:55:18 +0100

    aggiornamento foto

--- pages/ispirazioni.js
+++ pages/ispirazioni.js
@@ -13,7 +13,25 @@ const Ispirazioni = () => {
         backgroundClass="bg-[url('/assets/hero_ispirazioni.jpg')]"
       />
 
-      <Ispirazioni />
+      <div className="grid justify-between w-full grid-cols-1 gap-10 lg:grid-cols-2 h-[490px] my-20 px-6 lg:px-10">
+        <Link
+          href="/ispirazioni/bagno"
+          className="flex items-center justify-center w-full h-full bg-bagno rounded-[15px] lg:rounded-[40px] backdrop-blur-sm shadow-lg shadow-black/25"
+        >
+          <p className="px-5 lg:px-10 py-4 lg:py-[20px] text-xl lg:text-3xl font-bold backdrop-blur-sm bg-white/21 rounded-lg lg:rounded-[20px]">
+            Bagno
+          </p>
+        </Link>
+
+        <Link
+          href="/ispirazioni/living"
+          className="flex items-center justify-center w-full h-full bg-living rounded-[15px] lg:rounded-[40px] backdrop-blur-sm shadow-lg shadow-black/25"
+        >
+          <p className="px-5 lg:px-10 py-4 lg:py-[20px] text-xl lg:text-3xl font-bold backdrop-blur-sm bg-white/21 rounded-lg lg:rounded-[20px]">
+            Living
+          </p>
+        </Link>
+      </div>
       <FAQ />
       <StickyBanner />
     </>
